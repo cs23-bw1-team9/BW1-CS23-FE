fdbbdffcb4af6a3f7687e2ce4df47cac4f7d1b4c
matt-poloni <46501814+matt-poloni@users.noreply.github.com>
2019-11-21 15:15:00 -0500

    GamePage now grabs current room

--- adventure/src/Components/GamePage/GamePage.jsx
+++ adventure/src/Components/GamePage/GamePage.jsx
@@ -1,37 +1,43 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+// import Map from "./Map";
 
 const GamePage = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [map, setMap] = useState({});
   const [data, setData] = useState({});
-
-  const move = (e, direction) => {
-    e.preventDefault();
-    axios
-      .post("https://cs23-bw1-team9.herokuapp.com/api/adv/move/", {direction})
-      .then(res => setData(res.data))
-      .catch(err => console.error(err));
-  };
+  const [current, setCurrent] = useState({});
   
   useEffect(() => {
     setIsLoading(true);
-    axios
-      .get("https://cs23-bw1-team9.herokuapp.com/api/adv/init/")
+    axios.get("https://cs23-bw1-team9.herokuapp.com/api/adv/init/")
       .then(res => {
         setIsLoading(false);
         const {map, ...data} = res.data
         setMap(map)
         setData(data)
+        setCurrent(map.find(room => room.x === data.x && room.y === data.y))
       })
       .catch(err => {
-        console.error(err);
-        setIsLoading(false);
-        localStorage.removeItem("key");
+          console.error(err);
+          setIsLoading(false);
+          localStorage.removeItem("key");
       });
-  }, []);
+    }, []);
+
+  const move = (e, direction) => {
+    e.preventDefault();
+    axios.post("https://cs23-bw1-team9.herokuapp.com/api/adv/move/", {direction})
+      .then(res => setData(res.data))
+      .catch(err => console.error(err));
+  };
+
   return (
-    <p>GamePage</p>
+    <Map
+      map={map}
+      data={data}
+      current={current}
+    />
   )
 };
 
